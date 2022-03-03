46b09bfe1b872436d8411e7bffa12c3b2216dc86
adittiyan <92975787+adittiyan@users.noreply.github.com>
2022-03-04 00:11:35 +0700

    Add files via upload

---
+++ Praktikum (1)/SortArray.php
@@ -0,0 +1,24 @@
+  <!DOCTYPE html>
+ <html>
+ <body>
+
+ <?php
+ $ar_buah = ["p"=>"Pepaya","a"=>"Apel","m"=>"Mangga","j"=>"Jambu" ];
+ echo '<ol>';
+ foreach($ar_buah as $k =>$v){
+ echo '<li>'.$k.' - ' . $v .'</li>';
+ }
+ echo '</ol>';
+ sort($ar_buah);
+ echo '<hr/>';
+Web Programming 02| STT-NF
+ 7
+Praktikum 01: PHP Dasar
+ echo '<ol>';
+ foreach($ar_buah as $k =>$v){
+ echo '<li>'.$k.' - ' . $v .'</li>';
+ }
+ echo '</ol>';
+ ?>
+ </body>
+ </html>
